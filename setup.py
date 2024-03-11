d56736fae37127bd16ea15380675d260abd87c78
blueskyleader01 <blueskyleader01@hotmail.com>
2024-03-11 01:44:35 -0400

    feat: changed setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import find_packages, setup
 setup(
     name="ecrivai",
     packages=find_packages(),
-    # version='0.0.1'
-    version='0.0.2'
+    version='0.0.1'
+    # version='0.0.2'
 )
